06985faab26a5822573685b5d3d85906f0e88b1d
Berest_Oleg@mail.ru <Berest_Oleg@mail.ru>
2016-01-26 20:47:45 +0200

    Correction 26.01.2016 - add interfaces for all classes, change clone realization for Grade class.

---
+++ src/main/java/ua/berest/lab3/CourseImpl.java
@@ -0,0 +1,94 @@
+package ua.berest.lab3;
+
+/**
+ * Created by Oleg on 24.01.2016.
+ */
+
+public class CourseImpl implements Course {
+    private int courseId;
+    private String name;
+    private String description;
+    private String teacher;
+    private int locationId;
+
+    public CourseImpl(int courseId, String name, String description, String teacher, int locationId) {
+        this.courseId = courseId;
+        this.name = name;
+        this.description = description;
+        this.teacher = teacher;
+        this.locationId = locationId;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    @Override
+    public int hashCode() {
+        return toString().hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CourseImpl other = (CourseImpl) obj;
+        return !(locationId != other.locationId || courseId != other.courseId || name != other.name || !description.equals(description) || !teacher.equals(other.teacher));
+    }
+
+    @Override
+    protected Object clone() {
+        try{
+            CourseImpl copy = (CourseImpl)super.clone();
+            return copy;
+        }
+        catch(CloneNotSupportedException e){
+            throw new AssertionError("Impossible");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Course id = " + courseId + ", name = " + name + ", description = " + description + ", teacher = " + teacher + ", location id = " + locationId + ".";
+    }
+}
--- src/main/java/ua/berest/lab3/ModelDataAccess.java
+++ src/main/java/ua/berest/lab3/ModelDataAccess.java
@@ -7,15 +7,15 @@ import java.util.List;
  */
 public interface ModelDataAccess {
     List<Student> getAllStudents();
-    List<Student> getAllStudentsByCourse(Course course);
+    List<Student> getAllStudentsByCourse(CourseImpl course);
     List<Student> getAllStudentsByDepartment(int departmentLocationId);
     List<Student> getAllStudentsByCountry(int countryLocationId);
     List<Student> getAllStudentsByCity(int cityLocationId);
     List<Student> getAllStudentsByUniversity(int universityLocationId);
-    List<Course> getAllCourses();
+    List<CourseImpl> getAllCourses();
     List<Location> getAllCountries();
     List<Location> getAllCities();
     List<Location> getAllUniversities();
     List<Location> getAllDepartments();
-    List<Grade> getAllStudentCourseGrades(Course course, Student student);
+    List<GradeImpl> getAllStudentCourseGrades(CourseImpl course, Student student);
 }
